8901498daa4873b4e9ed7feaf99a7fe656d3af7d
highhigh <may.gxm@gmail.com>
2017-10-23 16:13:25 -0400

    submitted version

--- projects/smartcab/visuals.py
+++ projects/smartcab/visuals.py
@@ -62,12 +62,12 @@ def calculate_reliability(data):
 
 def plot_trials(csv):
 	""" Plots the data from logged metrics during a simulation."""
-
 	data = pd.read_csv(os.path.join("logs", csv))
 
+
 	if len(data) < 10:
-		print "Not enough data collected to create a visualization."
-		print "At least 20 trials are required."
+		print("Not enough data collected to create a visualization.")
+		print("At least 20 trials are required.")
 		return
 	
 	# Create additional features
